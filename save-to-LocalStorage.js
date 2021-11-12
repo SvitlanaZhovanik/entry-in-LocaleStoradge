06101336ce3965e3b2a8c35a6855f711cd910298
SvitlanaZhovanik <Lanasvetcat@gmail.com>
2021-11-12 12:36:40 +0200

    отрефакторен метод Инит

--- save-to-LocalStorage.js
+++ save-to-LocalStorage.js
@@ -1,41 +1,44 @@
 export default class MylocalStorageSave {
-constructor(classNameForm){
-   this.formRefs = document.querySelector(classNameForm),
-   this.key = '',
-   this.value = '',
-   this.form = '',
-   this.element = ''
-}
-saveToLS = (key, itemValue) => {
-    if (key === "password" || itemValue === ' '||itemValue === '') return
+  constructor(classNameForm) {
+    (this.formRefs = document.querySelector(classNameForm)),
+      (this.key = ''),
+      (this.value = ''),
+      (this.form = ''),
+      (this.element = ''),
+      (this.nodeList = []);
+  }
+  saveToLS = (key, itemValue) => {
+    if (key === 'password' || itemValue === ' ' || itemValue === '') return;
     localStorage.setItem(key, itemValue);
-}
-onInput = (evt) => {
+  };
+  onInput = evt => {
     this.key = evt.target.name;
-    this.value = evt.target.value
-    this.saveToLS(this.key,this.value);
-}
-onFormSubmit = (evt) => {
+    this.value = evt.target.value;
+    this.saveToLS(this.key, this.value);
+  };
+  onFormSubmit = evt => {
     evt.preventDefault();
     this.form = evt.target;
     localStorage.clear();
     this.form.reset();
-}
-getFromLocalStorage = (key) => {
-    this.element = document.querySelector(`.js-feedback-${key}`)
-       if (key) {
-        this.element.value = localStorage.getItem(key);
-       }
-   }
-loadListeners = ()=>{
-    this.formRefs.addEventListener("submit", this.onFormSubmit),
-    this.formRefs.addEventListener("input", this.onInput)
-}
+  };
+  getFromLocalStorage = key => {
+    this.element = this.formRefs.querySelector(`.js-feedback-${key}`);
+    if (key) {
+      this.element.value = localStorage.getItem(key);
+    }
+  };
+  loadListeners = () => {
+    this.formRefs.addEventListener('submit', this.onFormSubmit),
+      this.formRefs.addEventListener('input', this.onInput);
+  };
 
-init = ()=>{
+  init = () => {
     this.loadListeners();
-    this.getFromLocalStorage("username");
-    this.getFromLocalStorage("email");
-    this.getFromLocalStorage("message");
-}
+    this.nodeList = this.formRefs.querySelectorAll('input, textarea');
+
+    for (let list of this.nodeList) {
+      this.getFromLocalStorage(list.name);
+    }
+  };
 }
